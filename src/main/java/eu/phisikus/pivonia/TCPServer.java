fc68b7fb465f2a6ea904f4d91da831eb3c3f976e
Marcin Biernacki <phisikus@platinum.linux.pl>
2018-08-21 15:23:30 +0200

    Add simple DTO for messages, modify TCPServer to deserialize them (no tests so far)

--- src/main/java/eu/phisikus/pivonia/TCPServer.java
+++ src/main/java/eu/phisikus/pivonia/TCPServer.java
@@ -1,5 +1,9 @@
 package eu.phisikus.pivonia;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.undercouch.bson4jackson.BsonFactory;
+import io.vavr.control.Try;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -15,7 +19,7 @@ public class TCPServer implements AutoCloseable {
     private static final String ALL_INTERFACES_ADDRESS = "0.0.0.0";
     private AsynchronousServerSocketChannel serverSocket;
     private AsynchronousChannelGroup serverThreads;
-    private Queue<String> messageQueue = new ConcurrentLinkedQueue<>();
+    private Queue<Message> messageQueue = new ConcurrentLinkedQueue<>();
 
     public TCPServer(int port) throws IOException {
         this(ALL_INTERFACES_ADDRESS, port);
@@ -44,7 +48,7 @@ public class TCPServer implements AutoCloseable {
         serverThreads.shutdownNow();
     }
 
-    class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Queue<String>> {
+    class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Queue<Message>> {
         static final int COMMUNICATION_BUFFER_SIZE = 1024000;
         private AsynchronousServerSocketChannel serverSocket;
 
@@ -53,22 +57,23 @@ public class TCPServer implements AutoCloseable {
         }
 
         @Override
-        public void completed(AsynchronousSocketChannel clientChannel, Queue<String> messageQueue) {
+        public void completed(AsynchronousSocketChannel clientChannel, Queue<Message> messageQueue) {
             serverSocket.accept(null, this);
             var communicationBuffer = ByteBuffer.allocate(COMMUNICATION_BUFFER_SIZE);
             clientChannel.read(communicationBuffer, messageQueue, new ReadHandler(clientChannel, communicationBuffer));
         }
 
         @Override
-        public void failed(Throwable exc, Queue<String> attachment) {
+        public void failed(Throwable exc, Queue<Message> attachment) {
             System.out.println(exc);
         }
     }
 
-    class ReadHandler implements CompletionHandler<Integer, Queue<String>> {
+    class ReadHandler implements CompletionHandler<Integer, Queue<Message>> {
 
         private final AsynchronousSocketChannel clientChannel;
         private final ByteBuffer communicationBuffer;
+        private final ObjectMapper mapper = new ObjectMapper(new BsonFactory());
 
         public ReadHandler(AsynchronousSocketChannel clientChannel, ByteBuffer communicationBuffer) {
             this.clientChannel = clientChannel;
@@ -76,21 +81,21 @@ public class TCPServer implements AutoCloseable {
         }
 
         @Override
-        public void completed(Integer bytesReceived, Queue<String> messageQueue) {
-            if(bytesReceived == -1) {
+        public void completed(Integer bytesReceived, Queue<Message> messageQueue) {
+            if (bytesReceived == -1) {
                 System.out.println("Client disconnected. Current queue:");
                 messageQueue.forEach(System.out::println);
             }
             if (bytesReceived > 0) {
                 communicationBuffer.rewind();
-                String message = new String(communicationBuffer.array(), 0, bytesReceived);
-                messageQueue.add(message);
+                Try.of(() -> mapper.readValue(communicationBuffer.array(), Message.class))
+                        .map(messageQueue::add);
                 clientChannel.read(communicationBuffer, messageQueue, this);
             }
         }
 
         @Override
-        public void failed(Throwable exc, Queue<String> messageQueue) {
+        public void failed(Throwable exc, Queue<Message> messageQueue) {
             System.out.println(exc);
         }
     }
