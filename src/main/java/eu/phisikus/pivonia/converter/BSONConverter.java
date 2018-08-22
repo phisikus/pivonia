f646cf7bb28eb3f95cde520f9019b78ff68d400e
Marcin Biernacki <phisikus@platinum.linux.pl>
2018-08-22 21:33:41 +0200

    Add BSONConverter + test.

---
+++ src/main/java/eu/phisikus/pivonia/converter/BSONConverter.java
@@ -0,0 +1,23 @@
+package eu.phisikus.pivonia.converter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.undercouch.bson4jackson.BsonFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class BSONConverter {
+    private ObjectMapper mapper = new ObjectMapper(new BsonFactory());
+
+    public <T> byte[] serialize(T inputObject) throws IOException {
+        var output = new ByteArrayOutputStream();
+        mapper.writeValue(output, inputObject);
+        return output.toByteArray();
+    }
+
+    public <T> T deserialize(byte[] serializedObject, Class<T> objectType) throws IOException {
+        return mapper.readValue(serializedObject, objectType);
+    }
+
+
+}
---
+++ src/test/groovy/eu/phisikus/pivonia/converter/BSONConverterTest.groovy
@@ -0,0 +1,27 @@
+package eu.phisikus.pivonia.converter
+
+import eu.phisikus.pivonia.api.Message
+import spock.lang.Shared
+import spock.lang.Specification
+
+class BSONConverterTest extends Specification {
+
+    @Shared
+    def converter = new BSONConverter()
+
+    def "Object can be serialized to bytes and back"() {
+
+        given: "our test object is a message"
+        def expectedObject = new Message(42L, "TestTopic", "TestMessage")
+
+        when: "serialization is performed and the result is deserialized"
+        def serializedObject = converter.serialize(expectedObject)
+        def deserializedObject = converter.deserialize(serializedObject, Message.class)
+
+        then: "deserialized object is equal to the one that was serialized"
+        deserializedObject == expectedObject
+
+    }
+
+
+}
