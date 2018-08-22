dbcd8ce40044e399b63f2d111bbfbf39a96ac4af
Marcin Biernacki <phisikus@platinum.linux.pl>
2018-08-22 18:07:08 +0200

    Make TCP Client check connection before sending.

--- src/main/java/eu/phisikus/pivonia/TCPClient.java
+++ src/main/java/eu/phisikus/pivonia/TCPClient.java
@@ -9,28 +9,33 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
-public class TCPClient implements AutoCloseable{
+public class TCPClient implements AutoCloseable {
 
-    private final InetSocketAddress clientAddress;
-    private final SocketChannel clientChannel;
     private final ObjectMapper mapper = new ObjectMapper(new BsonFactory());
+    private SocketChannel clientChannel;
+
 
     public TCPClient(String address, int port) throws IOException {
-        clientAddress = new InetSocketAddress(address, port);
+        InetSocketAddress clientAddress = new InetSocketAddress(address, port);
         clientChannel = SocketChannel.open(clientAddress);
+        waitForReadyConnection();
+    }
+
+    public void send(Message message) throws IOException {
+        waitForReadyConnection();
+        ByteBuffer serializedMessageBuffered = getSerializedMessageAsBuffer(message);
+        while (serializedMessageBuffered.hasRemaining()) {
+            clientChannel.write(serializedMessageBuffered);
+        }
+    }
+
+    private void waitForReadyConnection() throws IOException {
         boolean waitForConnection = clientChannel.isConnectionPending() && clientChannel.finishConnect();
         if (!waitForConnection) {
             boolean channelIsConnected = clientChannel.isOpen() && clientChannel.isConnected();
             if (!channelIsConnected) {
                 throw new IOException("Channel is not open despite making an attempt to connect");
             }
-        }
-    }
-
-    public void send(Message message) throws IOException {
-        ByteBuffer serializedMessageBuffered = getSerializedMessageAsBuffer(message);
-        while (serializedMessageBuffered.hasRemaining()) {
-            clientChannel.write(serializedMessageBuffered);
         }
     }
 
