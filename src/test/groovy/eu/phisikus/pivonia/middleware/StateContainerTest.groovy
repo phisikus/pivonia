9744b429d4bea23c476fa0585a3023cb240809a3
Marcin Biernacki <phisikus@platinum.linux.pl>
2018-10-07 07:22:11 +0200

    Add StateContainer

---
+++ src/test/groovy/eu/phisikus/pivonia/middleware/StateContainerTest.groovy
@@ -0,0 +1,24 @@
+package eu.phisikus.pivonia.middleware
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class StateContainerTest extends Specification {
+    @Subject
+    def stateContainer = new StateContainer()
+
+    def "State container should store given object"() {
+        given: "sample object is created"
+        def sample = new Dummy()
+
+        when: "container is used to store object"
+        stateContainer.set(Dummy, sample)
+
+        then: "the object should be retrievable"
+        stateContainer.get(Dummy) == Optional.of(sample)
+
+    }
+
+    private class Dummy {
+    }
+}
