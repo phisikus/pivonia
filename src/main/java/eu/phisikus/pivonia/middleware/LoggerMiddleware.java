0872f4dda0cba8bdbd89b4aef506cc86748e5696
Marcin Biernacki <phisikus@platinum.linux.pl>
2018-10-13 01:19:14 +0200

    Add middleware for logging...

---
+++ src/main/java/eu/phisikus/pivonia/middleware/LoggerMiddleware.java
@@ -0,0 +1,46 @@
+package eu.phisikus.pivonia.middleware;
+
+import eu.phisikus.pivonia.api.middleware.Middleware;
+import eu.phisikus.pivonia.api.middleware.MiddlewareClient;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
+
+/**
+ * Middleware that uses provided logger to save information about incoming and outgoing messages.
+ *
+ * @param <T> type of message used in the system
+ */
+public class LoggerMiddleware<T> implements Middleware<T> {
+
+    private Level logLevel;
+    private Logger logger;
+
+    public LoggerMiddleware(Logger logger, Level logLevel) {
+        this.logLevel = logLevel;
+        this.logger = logger;
+    }
+
+    @Override
+    public void initialize(MiddlewareClient<T> middlewareClient) throws MissingMiddlewareException {
+        logger.log(logLevel, "Initializing LoggerMiddleware layer.");
+    }
+
+    @Override
+    public Optional<T> handleIncomingMessage(T message) {
+        logger.log(logLevel, "Incoming message: " + message);
+        return Optional.of(message);
+    }
+
+    @Override
+    public Optional<T> handleOutgoingMessage(T message) {
+        logger.log(logLevel, "Outgoing message: " + message);
+        return Optional.empty();
+    }
+
+    @Override
+    public void close() throws Exception {
+        logger.log(logLevel, "Shutting down LoggerMiddleware.");
+    }
+}
