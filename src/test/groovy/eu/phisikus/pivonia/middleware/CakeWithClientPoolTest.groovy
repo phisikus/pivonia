894101d0848459c39b98767fb988efbe84d8eeb3
Marcin Biernacki <phisikus@platinum.linux.pl>
2018-12-22 21:32:54 +0100

    Improve CakeWithClientPoolTest

--- src/test/groovy/eu/phisikus/pivonia/middleware/CakeWithClientPoolTest.groovy
+++ src/test/groovy/eu/phisikus/pivonia/middleware/CakeWithClientPoolTest.groovy
@@ -1,17 +1,15 @@
 package eu.phisikus.pivonia.middleware
 
 import eu.phisikus.pivonia.middleware.layer.pool.test.FakeMessage
-import spock.lang.Specification
 import spock.lang.Subject
 
-class CakeWithClientPoolTest extends Specification {
+class CakeWithClientPoolTest extends CakeIntegrationTest {
+
     @Subject
     def cake = new CakeWithClientPool(FakeMessage)
 
-    def "Should provide client pool"() {
+    def "Should compose client pool"() {
         expect: "Client pool to be available"
         cake.getClientPool() != null
     }
-
-    // TODO add real tests - inherit from other?
 }
