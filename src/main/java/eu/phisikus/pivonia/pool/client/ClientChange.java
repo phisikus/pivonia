7e2ed14a870eecbe26e67e4dcde54e087b186bd1
Marcin Biernacki <phisikus@platinum.linux.pl>
2019-01-17 09:27:55 +0100

    Extend ClientChange with some real fields

--- src/main/java/eu/phisikus/pivonia/pool/client/ClientChange.java
+++ src/main/java/eu/phisikus/pivonia/pool/client/ClientChange.java
@@ -1,4 +1,15 @@
 package eu.phisikus.pivonia.pool.client;
 
+import eu.phisikus.pivonia.api.Client;
+import lombok.Value;
+
+@Value
 public class ClientChange {
+
+    private final Client client;
+    private final Operation operation;
+
+    public enum Operation {
+        ADD, REMOVE
+    }
 }
