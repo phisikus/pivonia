327db1fdbc8d30b28ad12df237cb0f405ce36f5f
Marcin Biernacki <phisikus@platinum.linux.pl>
2019-01-31 13:11:20 +0100

    WIP: Add resilience4j library, use retry in ClientGenerator

--- src/main/java/eu/phisikus/pivonia/pool/mediators/ClientGenerator.java
+++ src/main/java/eu/phisikus/pivonia/pool/mediators/ClientGenerator.java
@@ -5,7 +5,11 @@ import eu.phisikus.pivonia.pool.AddressPool;
 import eu.phisikus.pivonia.pool.ClientPool;
 import eu.phisikus.pivonia.pool.address.Address;
 import eu.phisikus.pivonia.pool.address.AddressEvent;
+import io.github.resilience4j.retry.IntervalFunction;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import io.reactivex.disposables.Disposable;
+import io.vavr.control.Try;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -15,11 +19,18 @@ import javax.inject.Provider;
  * For each address added to the pool the manager will create a new connection and add the client to the client pool.
  */
 public class ClientGenerator implements Disposable {
+    private static String RETRY_ID = "client-generation-retry";
+    private RetryConfig retryConfiguration;
     private Disposable subscription;
 
-    // TODO complete, add tests
     @Inject
     public ClientGenerator(ClientPool clientPool, AddressPool addressPool, Provider<Client> clientProvider) {
+        retryConfiguration = RetryConfig.<Try>custom()
+                .maxAttempts(10)
+                .intervalFunction(IntervalFunction.ofExponentialBackoff())
+                .retryOnResult(Try::isFailure)
+                .build();
+
         bind(clientPool, addressPool, clientProvider);
     }
 
@@ -31,8 +42,10 @@ public class ClientGenerator implements Disposable {
     }
 
     private void handleAddressAddEvent(ClientPool clientPool, Provider<Client> clientProvider, Address address) {
-        var connectedClient = clientProvider.get().connect(address.getHostname(), address.getPort());
-        connectedClient.forEach(clientPool::add);
+        Retry.of(RETRY_ID, retryConfiguration)
+                .executeSupplier(() ->
+                        clientProvider.get().connect(address.getHostname(), address.getPort())
+                ).forEach(clientPool::add);
     }
 
     @Override
