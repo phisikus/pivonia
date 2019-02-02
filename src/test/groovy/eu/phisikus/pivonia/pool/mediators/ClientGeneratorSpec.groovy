530d5ae7936b44d9655d928bb657cd66b65d127e
Marcin Biernacki <phisikus@platinum.linux.pl>
2019-02-02 19:27:08 +0100

    Add test for client connection retry in ClientGenerator

--- src/test/groovy/eu/phisikus/pivonia/pool/mediators/ClientGeneratorSpec.groovy
+++ src/test/groovy/eu/phisikus/pivonia/pool/mediators/ClientGeneratorSpec.groovy
@@ -39,7 +39,42 @@ class ClientGeneratorSpec extends Specification {
         and: "connected using provided address"
         1 * client.connect('localhost', 7070) >> Try.success(client)
 
-        and: "it should be added to the Client Pool in a finite time interval"
+        and: "connected client should be added to the Client Pool in a finite time interval"
+        1 * clientPool.add(client)
+
+        cleanup: "client generator is destroyed"
+        generator.dispose()
+    }
+
+
+    def "Should retry to create and connect client after failure"() {
+        given: "Client Pool and Address Pool are provided"
+        def clientPool = Mock(ClientPool)
+        def addressPool = Mock(AddressPool)
+
+        and: "Client provider is prepared"
+        def client = Mock(Client)
+        def provider = Mock(Provider)
+
+        and: "Address Pool is configured to publish change events"
+        def addressChanges = PublishSubject.create()
+        addressPool.getAddressChanges() >> addressChanges
+
+        when: "Client Generator is created"
+        def generator = new ClientGenerator(clientPool, addressPool, provider)
+
+        and: "new Address is added to the Address Pool"
+        def newAddress = new Address("localhost", 7070)
+        addressChanges.onNext(new AddressEvent(AddressEvent.Operation.ADD, newAddress))
+
+        then: "Client provider should be called three times"
+        3 * provider.get() >> client
+
+        and: "there should be three connection attempts with only last one successful"
+        def failureResult = Try.failure(new IOException())
+        3 * client.connect('localhost', 7070) >>> [failureResult, failureResult, Try.success(client)]
+
+        and: "connected client should be added to the Client Pool in a finite time interval"
         1 * clientPool.add(client)
 
         cleanup: "client generator is destroyed"
