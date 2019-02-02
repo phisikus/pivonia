e4d64826bf4bf0268bee11ca5e3e14285f2f6e9c
Marcin Biernacki <phisikus@platinum.linux.pl>
2019-02-02 09:23:35 +0100

    WIP Improve ClientGeneratorSpec

--- src/test/groovy/eu/phisikus/pivonia/pool/mediators/ClientGeneratorSpec.groovy
+++ src/test/groovy/eu/phisikus/pivonia/pool/mediators/ClientGeneratorSpec.groovy
@@ -1,9 +1,12 @@
 package eu.phisikus.pivonia.pool.mediators
 
-
+import eu.phisikus.pivonia.api.Client
 import eu.phisikus.pivonia.pool.AddressPool
 import eu.phisikus.pivonia.pool.ClientPool
+import eu.phisikus.pivonia.pool.address.Address
+import eu.phisikus.pivonia.pool.address.AddressEvent
 import io.reactivex.subjects.PublishSubject
+import io.vavr.control.Try
 import spock.lang.Ignore
 import spock.lang.Specification
 
@@ -16,15 +19,37 @@ class ClientGeneratorSpec extends Specification {
         given: "Client Pool and Address Pool are provided"
         def clientPool = Mock(ClientPool)
         def addressPool = Mock(AddressPool)
+        def client = Mock(Client)
         def provider = Mock(Provider)
 
-        and: "Address Pool is configured"
+        and: "Client Pool is configured to register added clients"
         def clientChanges = PublishSubject.create()
-        clientPool.getClientChanges() >> clientChanges
+        clientPool.add(_ as Client) >> { parameters -> clientChanges.onNext(parameters.first()) }
 
-        when: "ClientGenerator is created"
+        and: "Address Pool is configured to publish change events"
+        def addressChanges = PublishSubject.create()
+        addressPool.getAddressChanges() >> addressChanges
+
+        when: "Client Generator is created"
         def generator = new ClientGenerator(clientPool, addressPool, provider)
 
-        then: "nothing" //TODO implement
+        and: "new Address is added to the Address Pool"
+        def hostname = "localhost"
+        def port = 7070
+        def newAddress = new Address(hostname, port)
+        addressChanges.onNext(new AddressEvent(AddressEvent.Operation.ADD, newAddress))
+
+        then: "Client should be generated"
+        1 * provider.get() >> client
+
+        and: "connected using provided address"
+        1 * client.connect(hostname, port) >> Try.success(client)
+
+        and: "it should be added to the Client Pool"
+        1 * clientPool.add(client)
+
+        cleanup: "client generator is destroyed"
+        generator.dispose()
+
     }
 }
