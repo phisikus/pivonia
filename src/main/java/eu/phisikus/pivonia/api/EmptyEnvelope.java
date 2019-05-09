e95b3915ba2d320c9d9c9f43f57bfc61031d4465
Marcin Biernacki <phisikus@platinum.linux.pl>
2019-05-09 20:17:06 +0200

    Make EmptyEnvelope not final

--- src/main/java/eu/phisikus/pivonia/api/EmptyEnvelope.java
+++ src/main/java/eu/phisikus/pivonia/api/EmptyEnvelope.java
@@ -1,8 +1,10 @@
 package eu.phisikus.pivonia.api;
 
 import lombok.Value;
+import lombok.experimental.NonFinal;
 
 @Value
+@NonFinal
 public class EmptyEnvelope<K> implements Envelope<K> {
     protected K senderId;
     protected K recipientId;
