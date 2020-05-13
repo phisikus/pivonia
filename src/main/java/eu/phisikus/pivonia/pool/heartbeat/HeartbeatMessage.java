a1714cc66be2309f5b65d208d2ee269fd9babf12
Marcin Biernacki <phisikus@platinum.linux.pl>
2020-05-13 23:31:19 +0200

    Add type info annotation to HeartbeatMessage

--- src/main/java/eu/phisikus/pivonia/pool/heartbeat/HeartbeatMessage.java
+++ src/main/java/eu/phisikus/pivonia/pool/heartbeat/HeartbeatMessage.java
@@ -1,9 +1,11 @@
 package eu.phisikus.pivonia.pool.heartbeat;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Value;
 
 @Value
 class HeartbeatMessage<K> {
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@className")
     private K senderId;
     private Long timestamp;
 }
