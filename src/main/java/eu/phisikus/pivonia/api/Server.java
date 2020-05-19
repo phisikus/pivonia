b0ff676b3f87019b4f42ff8c489b75bcd54bff8a
Marcin Biernacki <phisikus@platinum.linux.pl>
2020-05-19 13:54:46 +0200

    release 0.0.16: Add javadocs and source jar

--- src/main/java/eu/phisikus/pivonia/api/Server.java
+++ src/main/java/eu/phisikus/pivonia/api/Server.java
@@ -8,12 +8,12 @@ public interface Server extends Receiver, AutoCloseable {
      * Start listening on given port.
      *
      * @param port number of the port
-     * @return instance of the Server  that will handle the traffic
+     * @return instance of the Server that will handle the traffic
      */
     Try<Server> bind(int port);
 
     /**
-     * Start listening on given port & address.
+     * Start listening on given port and address.
      *
      * @param address address that the server should listen on
      * @param port    number of the port
